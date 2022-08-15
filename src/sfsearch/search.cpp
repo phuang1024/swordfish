7005131da1e43d2c277b8c6cae22e883fd0b5f5e
Patrick Huang <huangpatrick16777216@gmail.com>
2022-08-14 21:33:40 -0400

    Search stats.

--- src/sfsearch/search.cpp
+++ src/sfsearch/search.cpp
@@ -10,7 +10,7 @@ namespace Search {
 /**
  * Returns best score.
  */
-static int score_search(Position& pos, int depth) {
+static int score_search(Position& pos, int depth, ull& r_nodes) {
     if (depth == 0) {
         const int score = Eval::eval(pos);
         return pos.turn ? score : -score;
@@ -18,12 +18,13 @@ static int score_search(Position& pos, int depth) {
 
     std::vector<Move> moves;
     Movegen::get_legal_moves(pos, moves);
+    r_nodes += moves.size();
 
     int max_score = -1e9;
     for (const Move& move: moves) {
         Position new_pos = pos;
         new_pos.push(move);
-        const int score = -score_search(new_pos, depth-1);
+        const int score = -score_search(new_pos, depth-1, r_nodes);
         if (score > max_score)
             max_score = score;
     }
@@ -33,6 +34,8 @@ static int score_search(Position& pos, int depth) {
 
 
 SearchResult search(Position& pos, int depth) {
+    const ull time_start = Time::time();
+
     SearchResult res;
     res.data["depth"] = std::to_string(depth);
 
@@ -41,19 +44,24 @@ SearchResult search(Position& pos, int depth) {
 
     Move best_move;
     int best_score = -1e9;
+    ull nodes = 0;
     for (const Move& move: moves) {
         Position new_pos = pos;
         new_pos.push(move);
 
-        const int score = -score_search(new_pos, depth);
+        const int score = -score_search(new_pos, depth, nodes);
         if (score > best_score) {
             best_score = score;
             best_move = move;
         }
     }
 
+    const ull elapse = Time::elapse(time_start);
     res.data["pv"] = best_move.uci();
     res.data["cp"] = std::to_string(best_score);
+    res.data["nodes"] = std::to_string(nodes);
+    res.data["nps"] = std::to_string(Time::nps(nodes, elapse));
+    res.data["time"] = std::to_string(elapse);
     return res;
 }
 
