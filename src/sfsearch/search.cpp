39ed50d89acb79a2789b8e100b49a8708724a3d5
Patrick Huang <huangpatrick16777216@gmail.com>
2022-08-17 14:40:26 -0400

    Removed goto.

--- src/sfsearch/search.cpp
+++ src/sfsearch/search.cpp
@@ -36,14 +36,23 @@ static void unified_search(
     const int static_eval = Eval::eval_rel(pos, legal_moves.size());
 
     Move best_move(0, 0);
+    bool beta_cutoff = false;
 
     // End of game.
-    if (legal_moves.size() == 0)
-        goto exit;
+    if (legal_moves.size() == 0) {
+        r_eval = static_eval;
+        return;
+    }
 
-    // Quiesce search is started in exit.
-    if (!is_quiesce && remain_depth <= 0)
-        goto exit;
+    // Start quie search.
+    if (!is_quiesce && remain_depth <= 0) {
+        unified_search(
+                pos, maxdepth, mydepth + 1,
+                alpha, beta,
+                false, true,
+                r_eval, r_bestmove);
+        return;
+    }
 
     // Only used in quiesce.
     ull t_pieces;
@@ -51,7 +60,7 @@ static void unified_search(
         t_pieces = pos.relative_bb(pos.turn).t_pieces;
     }
 
-    // If no captures, return static eval.
+    // Start at static eval in case no captures for quie.
     if (is_quiesce)
         alpha = std::max(alpha, static_eval);
 
@@ -75,32 +84,21 @@ static void unified_search(
         curr_eval = -curr_eval;
 
         // Check alpha beta.
-        if (curr_eval >= beta)
-            goto exit;
+        if (curr_eval >= beta) {
+            beta_cutoff = true;
+            break;
+        }
         if (curr_eval > alpha) {
             alpha = curr_eval;
             best_move = move;
         }
     }
 
-exit:
-    // Common exit procedures.
+    // Exit procedures.
     if (is_root) {
         r_bestmove = best_move;
     }
-    if (legal_moves.size() == 0) {
-        r_eval = static_eval;
-    } else {
-        if (is_quiesce) {
-            r_eval = alpha;
-        } else {
-            unified_search(
-                    pos, maxdepth, mydepth + 1,
-                    alpha, beta,
-                    false, true,
-                    r_eval, r_bestmove);
-        }
-    }
+    r_eval = beta_cutoff ? beta : alpha;
 }
 
 
@@ -148,129 +146,6 @@ Move search(Position& pos, int maxdepth) {
     }
 
     return best_move;
-}
-
-
-/**
- * From https://www.chessprogramming.org/Quiescence_Search
- */
-static int quiesce_search(Position& pos, int alpha, int beta) {
-    std::vector<Move> moves;
-    Movegen::get_legal_moves(pos, moves);
-
-    // Return static eval if EOG.
-    int stand_pat = Eval::eval(pos, moves.size()) * (pos.turn ? 1 : -1);
-    if (moves.size() == 0)
-        return stand_pat;
-    if (stand_pat >= beta)
-        return beta;
-    if (stand_pat > alpha)
-        alpha = stand_pat;
-
-    const ull t_pieces = pos.relative_bb(pos.turn).t_pieces;
-
-    for (const Move& move: moves) {
-        if (!Bit::get(t_pieces, move.to))
-            continue;
-
-        Position new_pos = pos;
-        new_pos.push(move);
-
-        const int score = -quiesce_search(new_pos, -beta, -alpha);
-        if (score >= beta)
-            return beta;
-        if (score > alpha)
-            alpha = score;
-    }
-
-    return alpha;
-}
-
-
-/**
- * Returns best score.
- */
-static int score_search(TPTable& tptable, Position& pos, int maxdepth, int mydepth,
-        int alpha, int beta) {
-    const int remain_depth = maxdepth - mydepth;
-
-    std::vector<Move> moves;
-    Movegen::get_legal_moves(pos, moves);
-
-    // Return static eval if EOG.
-    int stand_pat = Eval::eval(pos, moves.size()) * (pos.turn ? 1 : -1);
-    if (moves.size() == 0)
-        return stand_pat;
-
-    const ull hash = Transposition::hash(pos);
-    TP* tp = tptable.get(hash);
-    bool tp_equal = tp->depth != -1 && pos == tp->pos;
-
-    // Check if position has been searched before.
-    if (tp_equal && tp->depth >= remain_depth)
-        return tp->score;
-
-    // Quiescence search if depth remaining is 0.
-    if (remain_depth <= 0) {
-        const int score = quiesce_search(pos, alpha, beta);
-        return score;
-    }
-
-    /* Move ordering
-    if (tp_equal) {
-        moves.push_back(tp->bestmove);
-    }*/
-
-    // Alpha beta search.
-    Move bestmove(0, 0);
-    for (int i = moves.size() - 1; i >= 0; i--) {
-        const Move& move = moves[i];
-        Position new_pos = pos;
-        new_pos.push(move);
-
-        const int score = -score_search(tptable, new_pos, maxdepth, mydepth+1, -beta, -alpha);
-        if (score >= beta)
-            return beta;
-        if (score > alpha) {
-            bestmove = move;
-            alpha = score;
-        }
-    }
-
-    // Write to transposition table.
-    if (bestmove.from != 0 && bestmove.to != 0 && remain_depth > tp->depth) {
-        tp->pos = pos;
-        tp->depth = mydepth;
-        tp->score = alpha;
-        tp->bestmove = bestmove;
-    }
-    return alpha;
-}
-
-
-static void root_search(TPTable& tptable, Position& pos, int maxdepth, int mydepth,
-        int alpha, int beta,
-        Move& r_bestmove, int& r_bestscore) {
-    std::vector<Move> moves;
-    Movegen::get_legal_moves(pos, moves);
-
-    for (const Move& move: moves) {
-        Position new_pos = pos;
-        new_pos.push(move);
-
-        const int score = -score_search(tptable, new_pos, maxdepth, mydepth+1, -beta, -alpha);
-        if (score >= beta) {
-            r_bestscore = beta;
-            r_bestmove = move;
-            return;
-        }
-        if (score > alpha) {
-            alpha = score;
-            r_bestmove = move;
-        }
-    }
-
-    r_bestscore = alpha;
 }
 
 
