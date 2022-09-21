010ddec3b9f25d81a2e471ad10062887a4efd788
Patrick Huang <huangpatrick16777216@gmail.com>
2022-09-21 07:51:48 -0700

    Display PV info last.

--- src/sfsearch/sfsearch.hpp
+++ src/sfsearch/sfsearch.hpp
@@ -22,10 +22,16 @@ public:
     inline std::string uci() {
         std::string str = "info ";
         for (const auto& [key, value]: data) {
+            if (key == "pv")   // PV must be last.
+                continue;
             str += key;
             str += " ";
             str += value;
             str += " ";
+        }
+        if (data.count("pv")) {
+            str += "pv ";
+            str += data["pv"];
         }
         return str;
     }
